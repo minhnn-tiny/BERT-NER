a4215b53d5e2069664c2db98246db029b35e967b
tanlongce <tanlong.ce@gmail.com>
2020-03-11 15:45:33 +0700

    add run predict script

---
+++ run_predict.sh
@@ -0,0 +1,13 @@
+python BERT_NER.py\
+    --task_name="NER"  \
+    --do_lower_case=False \
+    --crf=True \
+    --do_train=False   \
+    --do_eval=True   \
+    --do_predict=True \
+    --data_dir=CVA-data   \
+    --vocab_file=multi_cased_L-12_H-768_A-12/vocab.txt  \
+    --bert_config_file=multi_cased_L-12_H-768_A-12/bert_config.json \
+    --init_checkpoint=multi_cased_L-12_H-768_A-12/bert_model.ckpt   \
+    --max_seq_length=64   \
+    --output_dir=./output/result_dir
