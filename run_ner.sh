7ed8e2d3dc82673a1c7e4230cf8f4bc117b2d44e
tanlongce <tanlong.ce@gmail.com>
2020-03-11 15:35:08 +0700

    fix predict text from user function

--- run_ner.sh
+++ run_ner.sh
@@ -4,17 +4,17 @@
     --task_name="NER"  \
     --do_lower_case=False \
     --crf=True \
-    --do_train=True   \
+    --do_train=False   \
     --do_eval=True   \
     --do_predict=True \
-    --data_dir=data   \
-    --vocab_file=cased_L-12_H-768_A-12/vocab.txt  \
-    --bert_config_file=cased_L-12_H-768_A-12/bert_config.json \
-    --init_checkpoint=cased_L-12_H-768_A-12/bert_model.ckpt   \
-    --max_seq_length=128   \
+    --data_dir=CVA-data   \
+    --vocab_file=multi_cased_L-12_H-768_A-12/vocab.txt  \
+    --bert_config_file=multi_cased_L-12_H-768_A-12/bert_config.json \
+    --init_checkpoint=multi_cased_L-12_H-768_A-12/bert_model.ckpt   \
+    --max_seq_length=64   \
     --train_batch_size=32   \
     --learning_rate=2e-5   \
-    --num_train_epochs=4.0   \
+    --num_train_epochs=5.0   \
     --output_dir=./output/result_dir
 
 
